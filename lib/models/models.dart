9a860c7393aacd038e67c2211dc6f8249c61f9aa
Austin Caprio <austincaps98@gmail.com>
2022-11-29 19:15:08 -0700

    Models close to being done

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:json_annotation/json_annotation.dart';
 part 'models.g.dart';
 
@@ -7,22 +8,22 @@ part 'models.g.dart';
 // this is temporary, can be updated
 @JsonSerializable()
 class User {
-  String UserID;
-  String FirstName;
-  String LastName;
-  String Gender;
-  int Age;
-  String Biography;
-  String ImageURL;
+  String userID;
+  String firstName;
+  String lastName;
+  String gender;
+  int age;
+  String biography;
+  String imageURL;
 
-  Student({
-    required this.UserID,
-    required this.FirstName = '',
-    this.LastName = '',
-    this.Gender = '',
-    this.Age = 0,
-    this.Biography = '',
-    this.ImageURL = '',
+  User({
+    required this.userID,
+    required this.firstName,
+    required this.lastName,
+    this.gender = '',
+    this.age = 0,
+    this.biography = '',
+    this.imageURL = '',
   });
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
@@ -31,41 +32,60 @@ class User {
 
 @JsonSerializable()
 class Schedule {
-  String ScheduleID;
-  String UserID;
-  [] TripList;
+  String scheduleID;
+  String userID;
+  List<String> tripList;
 
   Schedule({
-    required this.ScheudleID,
-    required this.UserID = '',
-    this.TripList = [],
+    required this.scheduleID,
+    required this.userID,
+    this.tripList = const <String>[],
   });
 
   factory Schedule.fromJson(Map<String, dynamic> json) => _$ScheduleFromJson(json);
   Map<String, dynamic> toJson() => _$ScheduleToJson(this);
 
 }@JsonSerializable()
-class User {
-  String UserID;
-  String FirstName;
-  String LastName;
-  String Gender;
-  int Age;
-  String Biography;
-  String ImageURL;
+class Trip {
+  String tripID;
+  String locationID;
+  String scheduleID;
+  String userID;
+  String courseName;
+  int startingZone;
+  int endingZone;
+  DateTime departureTime;
 
-  Student({
-    required this.UserID,
-    required this.FirstName = '',
-    this.LastName = '',
-    this.Gender = '',
-    this.Age = 0,
-    this.Biography = '',
-    this.ImageURL = '',
+  Trip({
+    required this.tripID,
+    required this.locationID,
+    required this.scheduleID,
+    required this.userID,
+    this.courseName = '',
+    this.startingZone = 0,
+    this.endingZone = 0,
+    required this.departureTime
   });
 
-  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
-  Map<String, dynamic> toJson() => _$UserToJson(this);
+  factory Trip.fromJson(Map<String, dynamic> json) => _$TripFromJson(json);
+  Map<String, dynamic> toJson() => _$TripToJson(this);
+}
+
+@JsonSerializable()
+class Location {
+  String locationID;
+  String zoneID;
+  String locationName;
+
+
+  Location({
+    required this.locationID,
+    required this.zoneID,
+    this.locationName = '',
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json) => _$LocationFromJson(json);
+  Map<String, dynamic> toJson() => _$LocationToJson(this);
 }
 // EXAMPLE //
 // I always recommend making as few things required as possible
